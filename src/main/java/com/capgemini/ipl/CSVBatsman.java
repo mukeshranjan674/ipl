604aacf57120d35da2a780e5191d3ce76b27b87a
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-10-28 17:33:21 +0530

    [Mukesh] ADDED : Ability to find bowler with best average

--- src/main/java/com/capgemini/ipl/CSVBatsman.java
+++ src/main/java/com/capgemini/ipl/CSVBatsman.java
@@ -12,11 +12,11 @@ import com.opencsvbuilder.CSVException;
 import com.opencsvbuilder.ICSVBuilder;
 
 public class CSVBatsman {
-	public List<Batsman> loadBatsmanList(String BATSMAN_CSV_DATA) throws IPLAnalyserException {
-		try (Reader reader = Files.newBufferedReader(Paths.get(BATSMAN_CSV_DATA));) {
+	public List<Batsman> loadBatsmanList(String BATSMAN_CSV_PATH) throws IPLAnalyserException {
+		try (Reader reader = Files.newBufferedReader(Paths.get(BATSMAN_CSV_PATH));) {
 			ICSVBuilder icsvBuilder = CSVBuilderFactory.createCSVBuilder();
-			List<Batsman> stateCensusList = icsvBuilder.getCsvFileList(reader, Batsman.class);
-			return stateCensusList;
+			List<Batsman> batsmanList = icsvBuilder.getCsvFileList(reader, Batsman.class);
+			return batsmanList;
 		} catch (IOException e) {
 			throw new IPLAnalyserException(e.getMessage(), ExceptionType.FILE_NOT_FOUND);
 		} catch (CSVException e) {
