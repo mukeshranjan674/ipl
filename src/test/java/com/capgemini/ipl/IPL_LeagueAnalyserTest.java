2046724ad924aa5c2c773d712aa2efa8cb697ee5
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-10-28 15:20:07 +0530

    [Mukesh] ADDED : Test Case to find batsman with highest average written

--- src/test/java/com/capgemini/ipl/IPL_LeagueAnalyserTest.java
+++ src/test/java/com/capgemini/ipl/IPL_LeagueAnalyserTest.java
@@ -1,7 +1,31 @@
 package com.capgemini.ipl;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.gson.Gson;
+
 public class IPL_LeagueAnalyserTest {
-	public static void main(String[] args) {
-		System.out.println("Welcome to IPL League Analysis");
+	
+	private static final String BATSMAN_DATA_PATH = "./src/test/resources/BatsmanData.csv";
+	IPL_LeagueAnalyser ipl_LeagueAnalyser = null;
+	
+	@Before
+	public void setUp() {
+		ipl_LeagueAnalyser = new IPL_LeagueAnalyser();
+		try {
+			ipl_LeagueAnalyser.loadBatsmanData(BATSMAN_DATA_PATH);
+		} catch (IPLAnalyserException e) {
+		}
+	}
+	
+	@Test
+	public void givenCSVFile_shouldReturn_CricketersWith_TopBattingAverages() {
+		String sortedBatsmanData = ipl_LeagueAnalyser.getBestBattingAveragesCricketers();
+		Batsman[] sortedBatsmanArray = new Gson().fromJson(sortedBatsmanData, Batsman[].class);
+		System.out.println(sortedBatsmanArray[0].getAverage());
+		assertEquals("MS Dhoni", sortedBatsmanArray[0].getName());
 	}
 }
